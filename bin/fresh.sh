803ce8ba41cfdfb2d8a5b92eede6a332b266459b
Stephen Garver <stepgar@gmail.com>
2014-11-06 18:36:09 -0800

    Removing old wallaper image

--- bin/fresh.sh
+++ bin/fresh.sh
@@ -2,18 +2,22 @@
 echo "Warning: Your system will reboot once this script has completed!\n";
 sleep 3;
 
-cd ~;
-
 sudo apt-get update;
 sudo apt install ubuntu-sdk git vim vim-gtk build-essential unity-tweak-tool mplayer nvidia-331-updates zsh python-pip ranger htop;
 
 sudo pip install mps-youtube;
 
+git config --global user.email "stepgar@gmail.com";
+git config --global user.name "Stephen Garver";
+git config --global push.default simple;
+
 git clone https://github.com/robbyrussell/oh-my-zsh.git;
-mv oh-my-zsh .oh-my-zsh;
+mv oh-my-zsh ~/.oh-my-zsh;
+
 cp .oh-my-zsh/templates/zshrc.zsh-template .zshrc;
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="lambda"/g' .zshrc;
 sed -i 's/plugins=(git)/plugins=(git debian)/g' .zshrc;
+
 chsh -s /bin/zsh;
 
 curl -sL https://deb.nodesource.com/setup | sudo bash -;
