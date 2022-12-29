567bdb8eaed22d35d0b062de90fd587d82dd9ab2
Stephen Garver <stephen@garver.work>
2022-12-29 10:35:12 -0800

    Rebuild nvim config in lua (in-progress)

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,4 +1,3 @@
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local setKeyMaps = require('keymaps').setKeyMaps
 
 require('telescope').setup{
@@ -9,167 +8,28 @@ require('telescope').setup{
 
 require('telescope').load_extension('fzf')
 
--- lua
-require('lspconfig').sumneko_lua.setup {
-    capabilities = capabilities,
-    on_attach = function() print('LSP Attached: sumneko_lua')
-        setKeyMaps()
-    end,
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'},
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-}
-
+-- LSP
 -- install and manage LSP servers, DAP servers, linters, and formatters
-require('mason').setup()
-
--- javascript
-require('lspconfig').tsserver.setup{
-    capabilities = capabilities,
-    on_attach = function() print('LSP Attached: tsserver')
-        setKeyMaps()
-    end,
-}
-
--- css
-require('lspconfig').cssls.setup {
-    capabilities = capabilities,
-    on_attach = function() print('LSP Attached: cssls')
-        setKeyMaps()
-    end,
-}
-
--- golang
-require('lspconfig').gopls.setup{
-    capabilities = capabilities,
-    on_attach = function() print('LSP Attached: gopls')
-        setKeyMaps()
-    end,
-}
-
-
--- dotnet
-require('lspconfig').omnisharp.setup {
-    cmd = { "dotnet", "/home/tv3n/tools/omnisharp-linux-x64-net6.0/OmniSharp.dll" },
-
-    -- Enables support for reading code style, naming convention and analyzer
-    -- settings from .editorconfig.
-    enable_editorconfig_support = true,
-
-    -- If true, MSBuild project system will only load projects for files that
-    -- were opened in the editor. This setting is useful for big C# codebases
-    -- and allows for faster initialization of code navigation features only
-    -- for projects that are relevant to code that is being edited. With this
-    -- setting enabled OmniSharp may load fewer projects and may thus display
-    -- incomplete reference lists for symbols.
-    enable_ms_build_load_projects_on_demand = false,
-
-    -- Enables support for roslyn analyzers, code fixes and rulesets.
-    enable_roslyn_analyzers = false,
-
-    -- Specifies whether 'using' directives should be grouped and sorted during
-    -- document formatting.
-    organize_imports_on_format = false,
-
-    -- Enables support for showing unimported types and unimported extension
-    -- methods in completion lists. When committed, the appropriate using
-    -- directive will be added at the top of the current file. This option can
-    -- have a negative impact on initial completion responsiveness,
-    -- particularly for the first few completion sessions after opening a
-    -- solution.
-    enable_import_completion = false,
-
-    -- Specifies whether to include preview versions of the .NET SDK when
-    -- determining which version to use for project loading.
-    sdk_include_prereleases = true,
-
-    -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-    -- true
-    analyze_open_documents_only = false,
-
-    capabilities = capabilities,
-    on_attach = function() print('LSP Attached: omnisharp')
-       setKeyMaps()
-    end,
-}
-
--- completion
-vim.opt.completeopt={"menu", "menuone", "noselect"} -- setting vim config values
-
--- Set up nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup({
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      end,
-    },
-    window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
-    }, {
-      { name = 'buffer' },
-    })
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = { "sumneko_lua", "omnisharp", "rust_analyzer" }
 })
 
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
-      { name = 'buffer' },
-    })
-})
+require('fidget').setup()
 
---[[
--- fix me!!!
-require('nvim-dap-virtual-text').setup()
+local on_attach = function()
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
--- golang debugging
-require('dap-go').setup()
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
 
--- dapui debugging setup
-require('dapui').setup()
-local dap, dapui = require("dap"), require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
-]]
+-- servers to use
+require("lspconfig").sumneko_lua.setup { on_attach = on_attach }
+require("lspconfig").rust_analyzer.setup { on_attach = on_attach }
+require("lspconfig").omnisharp.setup { on_attach = on_attach }
+
 
