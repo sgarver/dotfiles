61c86d8c1bda865ee47cf0f56f85816d881fcb48
sgarver <stephen@garver.work>
2022-12-31 11:17:10 -0800

    Remove dap keymaps

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,8 +3,3 @@ vim.g.maplocalleader = ' '
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 
--- dap
-vim.keymap.set("n", "<leader>g", ":lua require'dap'.continue()<CR>")
-vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>")
-vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
-vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
